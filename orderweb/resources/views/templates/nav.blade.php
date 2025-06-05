07e95cc54889e3dd5a8feb4f0c4269246d76c110
Karol Dayana Arbelaez cortes <arbelaezcorteskaroldayana@gmail.com>
2025-06-05 11:35:04 -0500

    reportes P1
    se crea controlador y template para reportes, también css

--- orderweb/resources/views/templates/nav.blade.php
+++ orderweb/resources/views/templates/nav.blade.php
@@ -138,11 +138,12 @@
                     </div>
                 </li>
             @endcan
+
+            @can('administrador')
                 <!-- Heading -->
                 <div class="sidebar-heading">
                     Reportes
                 </div>
-
                 <li class="nav-item">
                     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse7"
                     aria-expanded="true" aria-controls="collapse7">
@@ -151,11 +152,11 @@
                     </a>
                     <div id="collapse7" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                         <div class="bg-white py-2 collapse-inner rounded">
-                            <a class="collapse-item" href="#">Consultar</a>
+                            <a class="collapse-item" href="{{ route('reports.index') }}">Consultar</a>
                         </div>
                     </div>
                 </li>
-           
+            @endcan
 
 
             <!-- Divider -->
