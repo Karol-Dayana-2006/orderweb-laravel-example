81f9438bb4229de34c5c4f739b4b5a1e2b2bc6dc
Karol Dayana Arbelaez cortes <arbelaezcorteskaroldayana@gmail.com>
2025-06-10 09:46:39 -0500

    reportes P2
    se termina reporte de técnicos y se crea reportes de actividades por técnico

--- orderweb/app/Http/Controllers/ReportController.php
+++ orderweb/app/Http/Controllers/ReportController.php
@@ -2,28 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\Technician;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
-
 {
-    public function index(){
-        return view('resports.index');
+    public function index()
+    {
+        $technicians = Technician::all();
+        return view('reports.index', compact('technicians'));
     }
 
     /**
      * reporte que genera el listado de todos los técnicos
      */
-
-    public function export_technician()
+    public function export_technicians()
     {
         $technicians = Technician::all();
         $data = array(
             'technicians' => $technicians
         );
-        $pdf = Pdf::loadView('reports.export_technicians', $data)->setPaper('letter', 'portrait'); //landscape: horizontal
+
+        $pdf = Pdf::loadView('reports.export_technicians', $data)
+            ->setPaper('letter', 'portrait')
+            ->setOptions([
+                'isRemoteEnabled' => true,
+                'defaultFont' => 'sans-serif'
+            ]); //landscape: horizontal
+
         return $pdf->download('technicians.pdf');
     }
+    
+    /**
+     * reporte que genera el listado de actividades de un técnico
+     */
+    public function export_activities_by_technician(Request $request)
+    {
+        $activities = Activity::where('technician_id', $request['technician_id'])->get();
+
+        $data = array(
+            'activities' => $activities
+        );
+
+        $pdf = Pdf::loadView('reports.export_activities_by_technician', $data)
+            ->setPaper('letter', 'portrait')
+            ->setOptions([
+                'isRemoteEnabled' => true,
+                'defaultFont' => 'sans-serif'
+            ]); //landscape: horizontal
+
+        return $pdf->download('ActivitiesByTechnician-'. $request['technician_id']. '.pdf');
+    }
 }
+
--- orderweb/resources/views/reports/export_technician.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('templates.base_reposts')
-@section('header', 'Reporte general de técnicos')
-@section('content')
-    
-
-@endsection
